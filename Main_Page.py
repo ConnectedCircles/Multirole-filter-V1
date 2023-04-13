5b382f7ec8d1f745c4dc9a86f91902d03934cc08
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-04-13 11:30:26 +0200

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -4,6 +4,6 @@ def app():
     st.title("Connected Circles Profile Multirole Filter V2")
     st.subheader("Property of Connected Circles")
     st.write("""This multirole app allows you to filter LinkedIn profiles by various categories including seniority, gender or location - filters 
-    which are either missing, or inaccurate in SalesNav. You can download the data either labeled, filtered or filtered profile URLs only, all as a .csv""")
+    which are either missing, or inaccurate in SalesNav. You can download the data either labeled, filtered or filtered profile URLs only, all as a .csv.""")
 if __name__ == "__main__":
     app()
