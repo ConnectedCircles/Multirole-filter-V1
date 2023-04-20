10f6d2dc6e287fa40a72c7f31fea6555837cb69e
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-04-20 11:28:24 +0200

    Update 04Universal_Filter.py

--- pages/04Universal_Filter.py
+++ pages/04Universal_Filter.py
@@ -14,38 +14,34 @@ def app():
     
 
     
+     # Define the list of substrings to search for
+    # Case sensitive substring
+    default_substringsCS = ['CEO', 'COO', 'CFO', 'CTO', 'CHRO', 'CMO', 'CLO', 'CSO', 'CIO', 'CTIO', 'CSIO', 'CCO', 'CDO', 'VP']
+    # Case insensitive substring 
+    default_substringsCI = ['Chief','Vice President', 'Vice-President', 'Partner', 'Owner', 'Founder','President','Partner']
+    
     # Get user input for substrings
-    substringsCS = st.text_input("Enter case-sensitive keywords separated by comma", "").split(",")
-    substringsCI = st.text_input("Enter case-insensitive keywords separated by comma", "").split(",")
+    substringsCS = st.text_input("Enter case-sensitive keywords separated by comma", ", ".join(default_substringsCS)).split(",")
+    substringsCI = st.text_input("Enter case-insensitive keywords separated by comma", ", ".join(default_substringsCI)).split(",")
+    
     # File uploader
     uploaded_file = st.file_uploader("Choose a CSV file to filter", type="csv")
-    
 
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-        
-        # Define the list of columns in the dataframe
-        columns_list = list(df.columns)
-        # Add a dropdown menu to select a column
-        column = st.selectbox('Select a column', options=columns_list)
-        
-        st.write('You selected:', column)
 
-        
-        
-
-        # Create a boolean mask to identify rows where the selected column contains any of the case-sensitive substrings
+        # Create a boolean mask to identify rows where the "Title" column contains any of the case-sensitive substrings
         maskCS = df['Title'].str.contains('|'.join(substringsCS))
 
-        # Create a boolean mask to identify rows where the selected column contains any of the case-insensitive substrings
+        # Create a boolean mask to identify rows where the "Title" column contains any of the case-insensitive substrings
         maskCI = df['Title'].str.contains('|'.join(substringsCI), case=False)
 
-        # Create a new column called "Filter Criteria" with a value of "Yes" for rows that match either condition, and "No" otherwise
-        df['Filter Criteria'] = (maskCS | maskCI).map({True: 'Yes', False: 'No'})
+        # Create a new column called "CXO+" with a value of "Yes" for rows that match either condition, and "No" otherwise
+        df['CXO+'] = (maskCS | maskCI).map({True: 'Yes', False: 'No'})
 
-        # Filter to only include Filter Criteria = Yes, delete "Filter Criteria" column
-        dffiltered = df[df["Filter Criteria"]=="Yes"]
-        dffiltered = dffiltered.drop("Filter Criteria", axis=1)
+        # Filter to only include CXO+, delete CXO+ column
+        dffiltered = df[df["CXO+"]=="Yes"]
+        dffiltered = dffiltered.drop("CXO+", axis=1)
 
         # Download link for filtered data
         csv_filtered = dffiltered.to_csv(index=False)
